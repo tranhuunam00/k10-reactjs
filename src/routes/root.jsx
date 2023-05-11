53fc2a0376de2426b9edae8f965a7bc5d7979452
namth1 <namth1@vuihoc.vn>
2023-05-11 15:31:05 +0700

    loading global

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,6 +5,7 @@ import {
   Form,
   redirect,
   NavLink,
+  useNavigation,
 } from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
 
@@ -20,6 +21,8 @@ export async function action() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
+
   return (
     <>
       <div id="sidebar">
@@ -70,7 +73,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div
+        id="detail"
+        className={navigation.state === "loading" ? "loading" : ""}
+      >
         <Outlet />
       </div>
     </>
