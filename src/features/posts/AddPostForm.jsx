6a514d59ee830aa394db27d998cdc6a7526a7aa3
namth1 <namth1@vuihoc.vn>
2023-05-16 16:21:20 +0700

    navigate

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -1,11 +1,36 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import { useNavigate } from "react-router-dom";
+
+import { postAdded } from "./postsSlice";
 
 export const AddPostForm = () => {
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
   const onTitleChanged = (e) => setTitle(e.target.value);
   const onContentChanged = (e) => setContent(e.target.value);
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          title,
+          content,
+        })
+      );
+
+      setTitle("");
+      setContent("");
+
+      navigate("/posts");
+    }
+  };
 
   return (
     <section>
@@ -26,7 +51,9 @@ export const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button">Save Post</button>
+        <button type="button" onClick={onSavePostClicked}>
+          Save Post
+        </button>
       </form>
     </section>
   );
