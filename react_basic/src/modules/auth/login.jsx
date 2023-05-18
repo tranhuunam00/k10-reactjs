3e26cf0680c7a019511ff9cfbf049a07aa4d2be6
namth <tranhuunam23022000@gmail.com>
2023-05-18 19:57:57 +0700

    filter component

--- react_basic/src/modules/auth/login.jsx
+++ react_basic/src/modules/auth/login.jsx
@@ -4,8 +4,49 @@ export function Clock({ color, time }) {
   return <h1 style={{ color: color }}>{time}</h1>;
 }
 
+const people = [
+  {
+    id: 0,
+    name: "Creola Katherine Johnson",
+    profession: "mathematician",
+  },
+  {
+    id: 1,
+    name: "Mario José Molina-Pasquel Henríquez",
+    profession: "chemist",
+  },
+  {
+    id: 2,
+    name: "Mohammad Abdus Salam",
+    profession: "physicist",
+  },
+  {
+    id: 3,
+    name: "Percy Lavon Julian",
+    profession: "chemist",
+  },
+  {
+    id: 4,
+    name: "Subrahmanyan Chandrasekhar",
+    profession: "astrophysicist",
+  },
+];
+
+const RenderName = ({ name, isRender }) => {
+  return <h1> {name}</h1>;
+};
+
+const userRender = people.filter(
+  (value, index) => value.profession === "chemist"
+);
 const LoginComponent = () => {
-  return <Clock time="hehe" color={"red"} />;
+  return (
+    <div>
+      {userRender.map((value) => (
+        <RenderName key={value.id} name={value.name} />
+      ))}
+    </div>
+  );
 };
 
 export default LoginComponent;
