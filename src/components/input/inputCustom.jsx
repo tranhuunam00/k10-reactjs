9a3e7bed4c1c5871596959172e6ae2e8a7e3ca31
namth <tranhuunam23022000@gmail.com>
2023-05-23 20:59:35 +0700

    d

--- src/components/input/inputCustom.jsx
+++ src/components/input/inputCustom.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import styles from "./style.module.scss";
 
-const InputCustom = ({ label, type, error }) => {
+const InputCustom = ({ label, type, error, onChange = () => {}, name }) => {
   const [value, setValue] = useState("");
 
   return (
     <div className={styles.inputCom}>
       <h5>{label}</h5>
       <input
-        onChange={(e) => {
-          console.log(e.target);
-        }}
+        name={name}
+        onChange={(e) => onChange(e.target)}
         type={type}
         placeholder="Nhập..."
       />
--- src/modules/auth/login.jsx
+++ src/modules/auth/login.jsx
@@ -3,15 +3,29 @@ import IMAGE_APP from "../../assets/image";
 import InputCustom from "../../components/input/inputCustom";
 
 const LoginPage = () => {
+  const [username, setUsername] = useState("");
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e;
+    if (name === "username") setUsername(value);
+  };
+
   return (
     <div>
       <h1>SIGN IN</h1>
       <img src={IMAGE_APP.iconMain} alt="" />
-      <InputCustom label={"Email"} type={"text"} error="error"></InputCustom>
+      <h1>{username}</h1>
+      <InputCustom
+        onChange={handleChangeInput}
+        label={"Email"}
+        type={"text"}
+        name={"username"}
+        error="error"
+      ></InputCustom>
       <InputCustom label={"Password"} type={"text"} error="error"></InputCustom>
       <button
         onClick={() => {
-          console.log("hjehe");
+          console.log(username);
         }}
       >
         submit
