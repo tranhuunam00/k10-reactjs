eb93c57ed26f2706287bf782b6ba5198ec2c56c5
namth <tranhuunam23022000@gmail.com>
2023-05-25 19:35:04 +0700

    input css

--- src/components/input/inputCustom.jsx
+++ src/components/input/inputCustom.jsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
 import styles from "./style.module.scss";
 
-const InputCustom = ({ label, type, error, onChange = () => {}, name }) => {
-  const [value, setValue] = useState("");
-
+const InputCustom = ({
+  label,
+  type,
+  error,
+  onChange = () => {},
+  name,
+  placeholder,
+  validate,
+}) => {
   return (
     <div className={styles.inputCom}>
-      <h5>{label}</h5>
-      <input
-        name={name}
-        onChange={(e) => onChange(e.target)}
-        type={type}
-        placeholder="Nhập..."
-      />
-      <p>{error}</p>
+      <label>{label}</label>
+      <div>
+        <input
+          name={name}
+          onChange={(e) => onChange(e.target)}
+          type={type}
+          placeholder={placeholder}
+          valid={validate}
+        />
+        <p className={styles.pEle}>{error}</p>
+      </div>
     </div>
   );
 };
