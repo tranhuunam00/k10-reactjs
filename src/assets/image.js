059845526d1291d3784219e89b3cc16d917117f5
namth <tranhuunam23022000@gmail.com>
2023-05-25 20:24:43 +0700

    username input

--- src/assets/image.js
+++ src/assets/image.js
@@ -1,10 +1,13 @@
 import iconMain from "./icons/iconMain.svg";
 import loginImage from "./images/loginImage.svg";
 import emailIcon from "./icons/emailIcon.svg";
+import userIcon from "./icons/userIcon.svg";
+
 const IMAGE_APP = {
   iconMain,
   loginImage,
   emailIcon,
+  userIcon,
 };
 
 export default IMAGE_APP;
--- src/modules/auth/register/register.jsx
+++ src/modules/auth/register/register.jsx
@@ -4,10 +4,14 @@ import InputCustom from "../../../components/input/inputCustom";
 import styles from "./style.module.scss";
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+
   const handleInput = (e) => {
     const { name, value, checked } = e;
     if (name === "email") setEmail(value);
+    if (name === "username") setUsername(value);
   };
+
   return (
     <div className={styles.register}>
       <div className={styles.register_info}>
@@ -19,6 +23,14 @@ const RegisterPage = () => {
             placeholder={"Nhập email... "}
             onChange={handleInput}
             icon={IMAGE_APP.emailIcon}
+            validate={"required|minLength:6"}
+          />
+          <InputCustom
+            name={"username"}
+            label={"UserName"}
+            placeholder={"Nhập username... "}
+            onChange={handleInput}
+            icon={IMAGE_APP.userIcon}
           />
         </div>
       </div>
