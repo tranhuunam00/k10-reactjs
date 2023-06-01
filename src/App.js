b449718e4bd669c2dbb798398f35072f7087f6e3
namth <tranhuunam23022000@gmail.com>
2023-06-01 19:18:04 +0700

    ref

--- src/App.js
+++ src/App.js
@@ -1,4 +1,20 @@
+import { useRef, useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+  const inputRef = useRef(null);
+
+  const [state, setState] = useState(0);
+
+  function handleClick() {
+    inputRef.current.focus();
+    console.log(inputRef.current);
+  }
+  return (
+    <>
+      <input ref={inputRef} />
+      <button onClick={handleClick}>Focus the input</button>
+    </>
+  );
+}
 export default App;
