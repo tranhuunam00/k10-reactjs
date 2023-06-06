99ec770b229c09a8ef2dc4b9aa19f4e6d15db185
namth <tranhuunam23022000@gmail.com>
2023-06-06 19:31:52 +0700

    edit

--- src/components/users/listUser/ListUser.jsx
+++ src/components/users/listUser/ListUser.jsx
@@ -1,13 +1,15 @@
 import styles from "./style.module.scss";
-const HeaderTable = ({ listItem }) => {
+const HeaderTable = ({ listItem, isCheck }) => {
   return (
     <div className={styles.row}>
-      <input
-        style={{
-          marginRight: 40,
-        }}
-        type="checkbox"
-      ></input>
+      {isCheck && (
+        <input
+          style={{
+            marginRight: 40,
+          }}
+          type="checkbox"
+        ></input>
+      )}
       {listItem.map((item) => (
         <h1
           style={{
@@ -20,22 +22,40 @@ const HeaderTable = ({ listItem }) => {
           {item.name}
         </h1>
       ))}
+      <h1
+        style={{
+          marginRight: 30,
+          width: 100,
+        }}
+      >
+        Edit
+      </h1>
+      <h1
+        style={{
+          marginRight: 30,
+          width: 100,
+        }}
+      >
+        Delete
+      </h1>
     </div>
   );
 };
 
-const BodyTable = ({ dataItem, listItem }) => {
+const BodyTable = ({ dataItem, listItem, isCheck = false }) => {
   return (
     <>
       {dataItem.map((item, index) => {
         return (
           <div key={index} className={styles.row}>
-            <input
-              style={{
-                marginRight: 40,
-              }}
-              type="checkbox"
-            ></input>
+            {isCheck && (
+              <input
+                style={{
+                  marginRight: 40,
+                }}
+                type="checkbox"
+              ></input>
+            )}
             {listItem.map((key, index) => {
               return (
                 <h3
@@ -47,6 +67,22 @@ const BodyTable = ({ dataItem, listItem }) => {
                 </h3>
               );
             })}
+            <button style={{ marginRight: 30, width: 100 }}>
+              <img
+                width="20"
+                height="20"
+                src="https://img.icons8.com/ios/50/edit--v1.png"
+                alt="edit--v1"
+              />
+            </button>
+            <button style={{ marginRight: 30, width: 100 }}>
+              <img
+                width="20"
+                height="20"
+                src="https://img.icons8.com/ios-glyphs/30/filled-trash.png"
+                alt="filled-trash"
+              />
+            </button>
           </div>
         );
       })}
@@ -65,8 +101,9 @@ const Table = () => {
   ];
   return (
     <div>
-      <HeaderTable listItem={listItem} />
+      <HeaderTable listItem={listItem} isCheck={true} />
       <BodyTable
+        isCheck={true}
         listItem={listItem}
         dataItem={[
           {
